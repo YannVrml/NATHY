195dc3f0579e139f4ad8aeac17101a34c779cc87
Nicolas Jacquemin <nicolas1.jacquemin@epitech.eu>
2024-03-22 23:57:34 +0100

    feat - add geoloc store and fullscreenchoices

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 import { RouterView } from 'vue-router'
 
-const drawer = ref(false);
+const drawer = ref(false)
 </script>
 
 <template>
@@ -14,14 +14,14 @@ const drawer = ref(false);
 
     <VNavigationDrawer v-model="drawer" temporary>
       <VList>
-        <VListItem :to="{name: 'home'}">Home</VListItem>
-        <VListItem :to="{name: 'about'}">About</VListItem>
+        <VListItem :to="{ name: 'home' }">Home</VListItem>
       </VList>
     </VNavigationDrawer>
 
     <VMain>
-      <RouterView />
+      <div class="d-flex h-100 align-center justify-center">
+        <RouterView />
+      </div>
     </VMain>
   </VApp>
-
 </template>
