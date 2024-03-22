d667f8bc906f170fac7425c84e84a1dcfd9a0c78
Nicolas Jacquemin <nicolas1.jacquemin@epitech.eu>
2024-03-22 19:27:36 +0100

    feat - hello world from vue

--- webapp/src/views/HomeView.vue
+++ webapp/src/views/HomeView.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+
+const username = ref('coucou');
+
+function getUsername() {
+  username.value = "nicolas";
+}
 </script>
 
 <template>
   <main>
     <h1>Home</h1>
+    <p>user: {{ username }}</p>
+    <VBtn @click="getUsername">Récuperer le username</VBtn>
   </main>
 </template>
+
+<style lang="scss" scoped>
+</style>
