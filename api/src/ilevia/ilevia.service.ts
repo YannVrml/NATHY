d3671f950b03d8af7c03a4c6ffff2716d8db4db3
Nicolas Jacquemin <nicolas1.jacquemin@epitech.eu>
2024-06-03 19:48:10 +0200

    hf - bypass ilevia security to api calls

--- api/src/ilevia/ilevia.service.ts
+++ api/src/ilevia/ilevia.service.ts
@@ -4,9 +4,20 @@ import { Injectable } from '@nestjs/common';
 export class IleviaService {
   private readonly ileviaBaseUrl = process.env.ILEVIA_API_BASE_URL;
 
+  private fetchOptions = {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+      'Referer': 'https://nws-lille.hove.io/fr/full/schedule',
+      'Accept': 'application/json',
+    }
+  }
+
   async lines() {
     const response = await fetch(
       `${this.ileviaBaseUrl}/full/ws/v1/lines?count=200`,
+      {
+        ...this.fetchOptions,
+      }
     );
     return response.json();
   }
@@ -14,6 +25,9 @@ export class IleviaService {
   async stopAreas(lineId: string) {
     const response = await fetch(
       `${this.ileviaBaseUrl}/full/ws/v1/lines/${lineId}/stop_areas?count=200`,
+      {
+        ...this.fetchOptions,
+      }
     );
     return response.json();
   }
@@ -21,6 +35,9 @@ export class IleviaService {
   async nextDepartures(lineId: string, stopPointId: string, routeId: string) {
     const response = await fetch(
       `${this.ileviaBaseUrl}/fr/full/journey/stop_areas/${stopPointId}/lines/${lineId}/routes/${routeId}/departures?count=10`,
+      {
+        ...this.fetchOptions,
+      }
     );
     return response.json();
   }
